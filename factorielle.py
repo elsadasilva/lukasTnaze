8f522278fcbd7f695baaba687865ca89f087b5e6
Vanmarcke Timothey <vanmarcke@a14p2.fil.univ-lille1.fr>
2018-10-19 09:24:15 +0200

    Ajout de a version non récursive

--- factorielle.py
+++ factorielle.py
@@ -5,3 +5,14 @@ def fact(n):
     else:
         return n*fact(n-1)
 
+"""
+Ajout de la version non récursive
+"""
+
+def fact(n):
+    """fact(n): calcule la factorielle de n (entier >= 0)"""
+    x=1
+    for i in xrange(2,n+1):
+        x*=i
+    return x
+
